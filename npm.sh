d430df2151446906c93a017d03a0c14caca65fc6
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2020-02-14 10:39:09 +0000

    Add cript to install clis via npm
    - SFDX cli
    - local webserver

---
+++ npm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Set permissions
+# echo "Setting npm permissions"
+# sudo chown -R $USER:$(id -gn $USER) $HOME/.config
+
+# Install commonly used clis
+echo "Downloading npm clis"
+npm i -g sfdx-cli
+npm i -g local-web-server
