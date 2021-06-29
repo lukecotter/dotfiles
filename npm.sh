a28ea7aa547802d147c70ee066dd26bf310ff91a
Luke Cotter <4013877+lukecotter@users.noreply.github.com>
2021-06-29 11:15:18 +0100

    feat(npm): install lts + latest  node + set latest to default

--- npm.sh
+++ npm.sh
@@ -5,7 +5,14 @@
 # sudo chown -R $USER:$(id -gn $USER) $HOME/.config
 
 # Install commonly used clis
+LATESTNODE=$(fnm ls-remote | cut -d "." -f1 | cut -d "v" -f2 | tail -1)
+echo "Installing node v$LATESTNODE + LTS"
+fnm install --lts
+fnm install $LATESTNODE
+fnm default $LATESTNODE
+
 echo "Downloading npm clis"
+npm i -g npm
 npm i -g sfdx-cli
 npm i -g local-web-server
 npm i -g vsce
